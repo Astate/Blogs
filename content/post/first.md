8e505270b3ac7cef1e9db09a12641fc0a750f8f8
homuncculus <16103127vaibhav@gmail.com>
2018-07-02 01:40:42 +0530

    added a utility script

--- content/post/first.md
+++
@@ -1,7 +0,0 @@
----
-title: "First Blog"
-date: 2018-07-01T17:03:28+05:30
-draft: false
----
-
-Hello, this is my first blog. Hoping to write more over time
---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+cd ../vaibhavk.github.io
+
+if [[ $(git status -s) ]]
+then
+    echo "The directory is dirty. Please commit any pending changes."
+    cd ../$(dirname "$0")
+    exit 1;
+fi
+# Add changes to git
+git add --all
+# Commit changes
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+
+git commit -m "$msg"
+# Push source and build repos.
+git push origin master
+
+# Come Back up to the Project Root
+cd ../$(dirname "$0")
